47214850bd94a61da2f1a2e46f1e10a6e55b1fb4
Damián Ponce <dami.ponce8@gmail.com>
2021-09-24 19:04:22 -0300

    Update next.config.js

--- src/next.config.js
+++ src/next.config.js
@@ -21,5 +21,7 @@ module.exports = {
       );
       return config;
    },
-   assetPrefix: isProd ? '/chat-analyser/' : '',
+   assetPrefix: isProd
+      ? 'https://cdn.statically.io/gh/damiponce//chat-analyser/gh-pages/'
+      : '',
 };
